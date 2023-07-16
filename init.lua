ebc47f907911c51eb6d867bfcc432ac7e6dca0a7
uuta <nightrage.metal66@gmail.com>
2023-07-16 18:27:12 +0900

    Added a hotkey to toggle a window in WezTerm

--- init.lua
+++ init.lua
@@ -1,8 +1,11 @@
 hs.loadSpoon("ShiftIt")
 spoon.ShiftIt:bindHotkeys({
-  left = {{ 'ctrl', 'cmd' }, 'left' },
-  right = {{ 'ctrl', 'cmd' }, 'right' },
-  up = {{ 'ctrl', 'cmd' }, 'up' },
-  down = {{ 'ctrl', 'cmd' }, 'down' },
-  maximum = {{ 'ctrl', 'cmd' }, 'm' },
+    left = {{'ctrl', 'cmd'}, 'left'},
+    right = {{'ctrl', 'cmd'}, 'right'},
+    up = {{'ctrl', 'cmd'}, 'up'},
+    down = {{'ctrl', 'cmd'}, 'down'},
+    maximum = {{'ctrl', 'cmd'}, 'm'}
 });
+
+hs.hotkey.bind({"ctrl"}, "L",
+               function() hs.application.launchOrFocus("WezTerm") end)
