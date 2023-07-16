a4bcc274565b65b7ecac000d86e09f37c56650a1
uuta <nightrage.metal66@gmail.com>
2023-07-16 18:22:38 +0900

    First commit!

---
+++ init.lua
@@ -0,0 +1,8 @@
+hs.loadSpoon("ShiftIt")
+spoon.ShiftIt:bindHotkeys({
+  left = {{ 'ctrl', 'cmd' }, 'left' },
+  right = {{ 'ctrl', 'cmd' }, 'right' },
+  up = {{ 'ctrl', 'cmd' }, 'up' },
+  down = {{ 'ctrl', 'cmd' }, 'down' },
+  maximum = {{ 'ctrl', 'cmd' }, 'm' },
+});
